ca8270a8bd25365088829b20d5ee2f64fb409f48
Lopez_HosheaShania <hosheashaniaclopez@iskolarngbayan.pup.edu.ph>
2026-01-10 14:11:12 +0800

    update changes

--- dcms/resources/views/dentist-inventory.blade.php
+++ dcms/resources/views/dentist-inventory.blade.php
@@ -86,7 +86,7 @@
       <span>Inventory</span>
     </a>
 
-    <a class="flex flex-col items-center opacity-60 hover:opacity-100">
+    <a href="{{ route('dentist.report') }}" class="flex flex-col items-center opacity-60 hover:opacity-100">
       <i class="fa-solid fa-file text-lg"></i>
       <span>Reports</span>
     </a>
