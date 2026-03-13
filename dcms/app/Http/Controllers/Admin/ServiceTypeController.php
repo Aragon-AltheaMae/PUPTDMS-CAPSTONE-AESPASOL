ed5f21a23e9c780457c8895d87ba1959a4ba7a51
Romero-DiannaRain <diannarain.romero31@gmail.com>
2026-03-13 11:39:14 +0800

    add service type feature in admin dashboard

---
+++ dcms/app/Http/Controllers/Admin/ServiceTypeController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\ServiceType;
+
+class ServiceTypeController extends Controller
+{
+
+    public function index()
+    {
+        $services = ServiceType::orderBy('name')->get();
+
+        return view('admin.service-types', compact('services'));
+    }
+
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255|unique:service_types,name'
+        ]);
+
+        ServiceType::create([
+            'name' => $request->name
+        ]);
+
+        return back()->with('success','Service type added');
+    }
+
+
+    public function destroy($id)
+    {
+        ServiceType::findOrFail($id)->delete();
+
+        return back()->with('success','Service type deleted');
+    }
+
+}
